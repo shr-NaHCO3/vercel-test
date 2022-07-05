87429dde60521bc8ce37ac3b6864ec720d1d0ad4
shr-NaHCO3 <shrshr2@163.com>
2022-07-05 12:48:39 +0800

    awa

---
+++ server.js
@@ -0,0 +1,15 @@
+const fastify = require('fastify')({
+    logger: true,
+})
+
+fastify.get('/', (req, res) => {
+    res.send('Hello world');
+})
+
+fastify.listen(3000, (err, ad)=>{
+    if(err){
+        fastify.log.err(err);
+        process.exit(1);
+    }
+    console.log('server is running')
+})
